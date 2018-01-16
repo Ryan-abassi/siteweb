486d5fbce514ee8a71c75050068464a030a76ab2
Zaouche Ryan <zaoucheryan@MacBook-Pro-de-Zaouche.local>
2018-01-16 14:00:31 +0100

    first commit

---
+++ projet.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css?family=Julius+Sans+One" rel="stylesheet"> 
+
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+	<header>
+
+
+     <div class="container-fluid">
+
+     	<div id="head" class="row">
+
+     		
+     			
+
+     			<img src="img/Ryan-logo.png" id="logo" class="col-md-2">
+
+     			<div class="offset-8 col-md-6 titre">
+				<h1>Ryan Zaouche Web Developer</h1>
+				</div>
+
+
+     			<nav class="navbar navbar-default" role="navigation" id="nav" class="col-md-4">
+
+     				<ul class="nav navbar-nav">
+     					<li class="active"><a href="Acceuil">Home</a></li>
+     					<li><a href="#about">About me</a></li>
+     					<li><a href="#abilities">Experience</a></li>
+     					<li><a href="#skills">Skills</a></li>
+     					<li><a href="#need">Need a developer ?</a></li>
+     				</ul>
+
+     			</nav>
+     	    
+
+
+		</div>
+
+
+
+     	
+     	
+
+	
+	
+     </div>
+		
+
+	</header>
+
+	<main>
+
+
+
+
+
+
+		<div class="container-fluid">
+			<div class="row">
+				<div id="about" class="col-md-12">
+					<h1 class="me">About Me :</h1>
+					<p class="para1"> Hello there ! I am Ryan , a 18 years old studient in a web developer school. I study there since 2017.</br>If you want someone serious and able to code your website(s) , I am the guy !</br>I live in Paris for now and i will maybe move to Canada in few years !  </p>
+					<p class="para2"> Born in Levallois-perret , I've always been hooked to computers and websites </br> After my highschool's graduation , I decided to evolve in the most natural way for me :</br> Web development ! </p>
+					<img src="img/quote-24.png" id="quote">
+					
+				</div>
+							
+		    </div>
+			
+		</div>
+
+
+
+
+		<div class="container-fluid">
+			<div class="row">
+
+				<div id="abilities" class="col-md-12">
+					<div id="fond" class="col-md-12">
+
+					<h1 id="titre3">Experience and Diplomas :</h1>
+
+					<ul id="liste-diplome">
+						<li><a>General certificate secondary education - 2013/2014</a></li>
+						<li><a>High school Diploma - 2016/2017</a></li>
+						<li><a>Master for computer engineering (in progress) - 2017/2022</a></li>
+					</ul>
+
+					<ul id="liste-skills">
+						<li><a>Professor's assistant for a one week-stage 2013/2014</a></li>
+						<li><a>Coder for few websites (personnal projects and orders)</a></li>
+						<li><a>Nice english level </a></li>
+					</ul>
+
+					<img src="img/graduation.png" class="icone">
+
+
+					</div>
+
+				</div>
+
+			</div>
+			
+		</div>
+
+
+		<div class="container-fluid">
+			<div class="row">
+
+
+				<div id="skills" class="col-md-12">
+
+					<p class="titre5">Skills :</p>
+					<p class="titre6">HTML5</p>
+					<p class="titre7">CSS</p>
+					<p class="titre8">JAVA</p>
+					<p class="titre9">JAVA-SCRIPT</p>
+
+					<img src="img/student.png" class="student">
+
+				</div>
+
+
+
+			</div>
+
+
+		</div>
+
+
+
+		<div class="container-fluid">
+
+			<div class="row">
+
+
+				<div id="need" class="col-md-12">
+
+
+					
+				<div id="fond4">
+						                
+					<h1 id="titre10">Need a developer ?</h1>
+					<p class="para3">If you need me , I will be happy to help you building the website of your dreams !</br> To contact me please choose between sending me a mail , or visit my Linkedin page !</p>
+					<a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fryan-zaouche-742742154%2F&h=ATNufnLhK1sRB5YbV-EpKFhvdJh7nIhdqIDtPVFoeha8hUhPwHa65NsJjwiByBGHGBXo4uk3s2CGbUYCD4oWLyABpFe3OC9kIGxSaRu6qx66OaIhqrkIAmvqgZhkLlyfd8zncCeOpgmTsrOdTYs" target="_blank"><img src="img/linkedin.png" class="icaune"></a>
+					<a href="mailto:ryanab@outlook.fr" target="_blank"><img src="img/email.png" id="iconeu"></a>
+
+					
+
+	            </div>
+
+				</div>
+				
+
+
+			</div>
+			
+
+
+
+
+
+
+
+
+
+		</div>
+
+
+
+
+
+
+
+
+
+
+		
+	</main>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
